f820f5b731ea7395463e4b3f6c6db161e32ef7d2
Josh Millard <josh.millard10@gmail.com>
2022-01-03 18:59:17 -0500

    test image added

---
+++ corbin_the_cabin/src/App.js
@@ -0,0 +1,12 @@
+import picture from './picture.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={picture} alt="picture" />
+    </div>
+  );
+}
+
+export default App;
