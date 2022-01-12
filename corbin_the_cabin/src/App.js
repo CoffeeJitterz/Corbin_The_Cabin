15d874599f5612775a3593149fc3cc086c35a3f9
Josh Millard <josh.millard10@gmail.com>
2022-01-12 18:11:02 -0500

    react-router-dom installed and about page implimented

--- corbin_the_cabin/src/App.js
+++ corbin_the_cabin/src/App.js
@@ -1,13 +1,25 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import AboutPage from './components/Pages/AboutPage'
 import Header from './components/Header';
 import Player from './components/Player'
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className='app'>
       <Header />
-      <Player />
+      <Routes>
+        <Route path='/' element={
+          <Player />
+        } />
+        <Route path='/about' element={
+          <AboutPage />
+        } />
+      </Routes>
     </div>    
+    </Router>
   );
 }
 
--- corbin_the_cabin/src/components/Header.jsx
+++ corbin_the_cabin/src/components/Header.jsx
@@ -1,7 +1,11 @@
+import {Link} from 'react-router-dom'
+
 function Header() {
   return (
     <div className='header_container'>
       <h1 className='header'>Corbin the Cabin .com</h1>
+      <Link to='/about'>About</Link>
+      <Link to='/'>Home</Link>
     </div>
   )
 }
