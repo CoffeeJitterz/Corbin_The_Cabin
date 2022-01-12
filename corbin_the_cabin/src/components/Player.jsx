26ec4e7be68ed02d3c049470da2c8d27223573e8
Josh Millard <josh.millard10@gmail.com>
2022-01-11 19:22:38 -0500

    data.js data object created and passed to player.jsx

--- corbin_the_cabin/src/components/Player.jsx
+++ corbin_the_cabin/src/components/Player.jsx
@@ -1,13 +1,16 @@
 import forest from '../images/forest.png';
+import data from '../data/data';
 
 function Player() {
+  console.log(data)
+  
   return (
     <div className="player">
       <div className='image_container'>
-        <img src={forest} alt="Forest"/>
+        <img src={data[0].image} alt="Forest"/>
       </div>
       <div className="day_count">
-        <h3>Day 1</h3>    
+        <h3>Day {data[0].day}</h3>    
       </div>
     </div>
 
---
+++ corbin_the_cabin/src/data/data.js
@@ -0,0 +1,10 @@
+import forest from '../images/forest.png'
+
+const data = [
+  {
+    day: '1',
+    image: forest,
+  }
+]
+
+export default data
