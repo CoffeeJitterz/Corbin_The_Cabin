429fb989839366aef3f696146223664b6f268b61
Josh Millard <josh.millard10@gmail.com>
2022-01-14 19:41:50 -0500

    New images added

--- corbin_the_cabin/src/data/data.js
+++ corbin_the_cabin/src/data/data.js
@@ -1,6 +1,8 @@
-import day0 from '../images/Day_0.png'
-import day1 from '../images/Day_1.png'
-import day2 from '../images/Day_2.png'
+import day0 from '../images/day0.png'
+import day1 from '../images/day1.png'
+import day2 from '../images/day2.png'
+import day3 from '../images/day3.png'
+import day4 from '../images/day4.png'
 
 const data = [
   {
@@ -14,6 +16,14 @@ const data = [
   {
     day: 2,
     image: day2,
+  },
+  {
+    day: 3,
+    image: day3,
+  },
+  {
+    day: 4,
+    image: day4,
   },
 ]
 
